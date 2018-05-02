55a6801da1ace53bdd953e5045ac25cd3eb57e49
taftsa <30739359+taftsa@users.noreply.github.com>
2018-05-01 21:32:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ var comics = [
 	{number:'032', title:'TTODBDILBRCE', date:'4/10/2018', hover:'Weirdest study session ever.'},
 	{number:'033', title:'Word Definitions', date:'4/17/2018', hover:'Moral of the story: Don\'t define words.'},
 	{number:'034', title:'Ghost Phone', date:'4/24/2018', hover:'There\'s not a clock there. I don\'t know why I drew a clock there.'},
+	{number:'035', title:'Grey\'s Anatomy', date:'5/1/2018', hover:'Caveat: Grey is still alive (for now).'},
 ];
 
 $(document).on("click", "#prev", function(){
