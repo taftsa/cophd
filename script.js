1cc4ff9b706e4f2c05c7563a305c8e42b189242c
taftsa <30739359+taftsa@users.noreply.github.com>
2018-10-16 16:14:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ var comics = [
 	{number:'042', title:'Life, the Universe, and Everything', date:'9/25/2018', hover:'And definitely don\'t write a comic advertising it.'},
 	{number:'043', title:'Rocky Mountain National Highway', date:'10/02/2018', hover:'To see paradise / I just need a parking spot'},
 	{number:'044', title:'We Interrupt This "Comic"', date:'10/9/2018', hover:'It\'s REALLY important, guys.'},
+	{number:'045', title:'Fantasy Football', date:'10/16/2018', hover:'Actually not the worst team; 7th out of 10 at the moment!'},
 ];
 
 $(document).on("click", "#prev", function(){
