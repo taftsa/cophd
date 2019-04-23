6d8d988b5830a6593fc9e35ee0dc05a78c90eae0
taftsa <30739359+taftsa@users.noreply.github.com>
2019-04-23 13:51:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -65,6 +65,7 @@ var comics = [
 	{number:'063', title:'Spring Break', date:'4/2/2019', hover:'Our office has carpeted walls. That\'s not related, but...'},
 	{number:'064', title:'Toronto, ON', date:'4/9/2019', hover:'Norfolk, VA has a Tim Hortons, and I\'m excited about it.'},
 	{number:'065', title:'Thanks, Statistics!', date:'4/16/2019', hover:'Statistics are fun. No, seriously!'},
+	{number:'066', title:'Easter Lunch', date:'4/23/2019', hover:'Good poutine, though.'},
 ];
 
 $(document).on("click", "#prev", function(){
